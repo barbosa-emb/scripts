da0e23715b24787762d8b82952698190f3968653
Eduardo Barbosa <eduardo.barbosa@araforros.com.br>
2020-01-28 23:05:23 -0300

    Refatoração

--- configureAndroidWifi.py
+++ configureAndroidWifi.py
@@ -1,6 +1,8 @@
 import os, sys 
 
-os.system("C:/Users/DTI001/AppData/Local/Android/SDK/platform-tools/adb devices")
-os.system("C:/Users/DTI001/AppData/Local/Android/SDK/platform-tools/adb kill-server")
-os.system("C:/Users/DTI001/AppData/Local/Android/SDK/platform-tools/adb tcpip 5555")
-os.system("C:/Users/DTI001/AppData/Local/Android/SDK/platform-tools/adb connect 192.168.0.22")
+cPath = "C:/Users/DTI001/AppData/Local/Android/SDK/platform-tools/adb "
+
+os.system(cPath + "devices")
+os.system(cPath + "kill-server")
+os.system(cPath + "tcpip 5555")
+os.system(cPath + "connect 192.168.0.22")
